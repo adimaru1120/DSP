1b387e6a633b81b2b22a88360ff6831c93f77c29
Adrian Roman <adimaru1120@gmail.com>
2021-12-13 19:35:42 +0100

    adding arange and save_to_file functions, checking results of generation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include "math.h"
+#include <fstream>
+
+#include "lib/inc/cordic.h"
+
+template<typename T>
+std::vector<T> arange(T start, T stop, T step = 1) {
+    std::vector<T> values;
+    for (T value = start; value < stop; value += step)
+        values.push_back(value);
+    return values;
+}
+
+void save_to_file(std::vector<std::vector<float>> re){
+    std::ofstream f_save;
+    f_save.open("cos.txt", std::ios_base::out);
+    for(int a = 0; a < re[0].size(); ++a)
+
+            f_save << re[0][a] << "\n";
+
+    f_save.close();
+
+    f_save.open("sin.txt", std::ios_base::out);
+    for(int a = 0; a < re[0].size(); ++a)
+
+            f_save << re[1][a] << "\n";
+
+    f_save.close();
+}
 
 int main(){
 
-    std::cout << "Hello World!!";
     
+    std::vector<float> theta = arange<float>(-1, 1, pow(2, -8));
+
+    cordic dsp;
+    std::vector<std::vector<float>> res = dsp.sin_cos_generator(1, 0, theta, 12);
+    save_to_file(res);
+    
+
     return 0;
 }
