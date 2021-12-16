db3d0f0b11600fedf0bed81864b72b72f9cd35e3
Adrian Roman <adimaru1120@gmail.com>
2021-12-16 15:21:21 +0100

    changing implementation of cordic_rotation (able on arbitrary angle not only in -PI/2 - PI/2) and sin_cos_generator

--- lib/src/cordic.cpp
+++ lib/src/cordic.cpp
@@ -3,7 +3,6 @@
 #include "math.h"
 #include "../inc/cordic.h"
 
-
 float cordic::set_gain(int n){
     float gain = 1;
 
@@ -17,56 +16,73 @@ float cordic::set_gain(int n){
 }
 
 void cordic::set_LUT(int n){
-    float PI = 3.14159265359f;
+
     for(int i = 0; i < n; ++i){
-        cordic::LUT.push_back(atanf(pow(2, -i)) / (PI / 2));
+        float angle = atanf(pow(2, -i));
+        cordic::LUT.push_back(angle);
     }
+
 }
 
-std::vector<float> cordic::cordic_rotation(float x, float y, float z, int n){
-    float xt = x;
-    float yt = y;
 
-    for(int i = 0; i < n; ++i){
+std::vector<float> cordic::cordic_rotation(float z, int n){   
 
+    if(z < -cordic::PI / 2 || z > cordic::PI / 2){
+        std::vector<float> rotated;
         if(z < 0){
-            z = z + cordic::LUT[i];
-            x = x + yt;
-            y = y - xt;
+            rotated = cordic::cordic_rotation(z + cordic::PI, n);
         }
         else{
-            z = z - cordic::LUT[i];
-            x = x - yt;
-            y = y + xt;
+            rotated = cordic::cordic_rotation(z - cordic::PI, n);
         }
 
-        xt = x * pow(2, -(i + 1));
-        yt = y * pow(2, -(i + 1));
+        rotated[0] = -rotated[0];
+        rotated[1] = -rotated[1];
+        return rotated;
     }
 
-    std::vector<float> rotated{x, y, z};
+    float x = 1;
+    float y = 0;
+
+    float xt;
+    float yt;
+
+    for(int i = 0; i < n; ++i){
+        if(z < 0){
+            xt = x + (y * pow(2, -i));
+            yt = y - (x * pow(2, -i));
+            z = z + cordic::LUT[i];
+        }
+        else{
+            xt = x - (y * pow(2, -i));
+            yt = y + (x * pow(2, -i));
+            z = z - cordic::LUT[i]; 
+        }
+
+        x = xt; y = yt;
+    }
+    
+    std::vector<float> rotated{x, y};
 
     return rotated;
 
 }
 
-
-std::vector<std::vector<float>> cordic::sin_cos_generator(float x, float y, std::vector<float> z, int n){
+std::vector<std::vector<float>> cordic::sin_cos_generator(std::vector<float> z, int n){
     float gain = cordic::set_gain(n);
-    cordic::set_LUT(n);
 
     std::vector<float> xt;
     std::vector<float> yt;
-    std::vector<float> zt;
 
     for(int i = 0; i < z.size(); ++i){
-        std::vector<float> rot = cordic::cordic_rotation(gain, y, z[i], n);
+        std::vector<float> rot = cordic::cordic_rotation(z[i], n);
+        rot[0] = rot[0] * gain;
+        rot[1] = rot[1] * gain;
         xt.push_back(rot[0]);
         yt.push_back(rot[1]);
-        zt.push_back(rot[2]);
     }
 
-    std::vector<std::vector<float>> sin_cos{xt, yt, zt};
+    std::vector<std::vector<float>> sin_cos{xt, yt};
 
     return sin_cos;
 }
